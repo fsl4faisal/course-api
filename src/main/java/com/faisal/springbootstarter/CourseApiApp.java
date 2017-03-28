8e1d6fc4321a81636101cea2c47179626ce2c63d
Md Faisal <fsl4faisal@gmail.com>
2017-03-29 00:18:59 +0530

    changed package names

--- src/main/java/com/faisal/springbootstarter/CourseApiApp.java
+++ src/main/java/com/faisal/springbootstarter/CourseApiApp.java
@@ -1,4 +1,4 @@
-package io.javabrains.springbootstarter;
+package com.faisal.springbootstarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/faisal/springbootstarter/hello/HelloController.java
+++ src/main/java/com/faisal/springbootstarter/hello/HelloController.java
@@ -1,4 +1,4 @@
-package io.javabrains.springbootstarter.hello;
+package com.faisal.springbootstarter.hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
